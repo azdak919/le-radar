a10154504979597d4567a52522a0699efe073095
Azdak <chtis6@ulaval.ca>
2026-07-23 21:32:33 -0400

    Forcer le rendu des icônes météo principales

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "radar-shell-v454";
+const CACHE_NAME = "radar-shell-v455";
 const CACHE_PREFIX = "radar-";
 // Isolated mini-apps under /pomo/ and /solitaire/ own their own SWs + caches.
 const ISOLATED_PATH_RE = /\/(pomo|solitaire)(\/|$)/;
